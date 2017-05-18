dee7bf13741cc5fe0bf6472e4aabb4c95aab60fd
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2017-05-17 21:20:26 -0500

    testing app

---
+++ seed/js/todo.controller.js
@@ -0,0 +1,32 @@
+function TodoController() {
+  this.newTodo = "";
+
+
+
+  this.list = [{
+    title: 'First todo item!',
+    completed: false
+  }, {
+    title: 'Second todo item!',
+    completed: false
+  }, {
+    title: 'Second todo item!',
+    completed: false
+  }];
+
+  this.addTodo = function () {
+    this.list.unshift({
+      title : this.newTodo,
+      completed : false
+    });
+
+    this.newTodo = "";
+  };
+    this.removeTodo = function (item, index){
+        this.list.splice(index, 1);
+    }
+}
+
+angular
+    .module('app')
+    .controller('TodoController', TodoController);
